79dbda46a07d627990597cec187055135d9a6f8d
Alexander Makarov <sam@rmcreative.ru>
2019-04-09 22:06:22 +0300

    Added language to formatter format method

--- src/Formatter.php
+++ src/Formatter.php
@@ -6,5 +6,5 @@ namespace Yii\I18n;
 
 interface Formatter
 {
-    public function format(string $message, array $parameters): string;
+    public function format(string $message, array $parameters, string $language): string;
 }
