6afe48a393f1b98d9a0c3981bc952787b439730b
Dmitriy Derepko <xepozz@list.ru>
2019-11-09 17:00:28 +0300

    Updated phpdoc

--- src/MessageFormatterInterface.php
+++ src/MessageFormatterInterface.php
@@ -4,7 +4,10 @@ namespace Yiisoft\I18n;
 interface MessageFormatterInterface
 {
     /**
-     * @throws FormattingException
+     * @param string $message
+     * @param array $parameters
+     * @param string $language
+     * @return string
      */
     public function format(string $message, array $parameters, string $language): string;
 }
